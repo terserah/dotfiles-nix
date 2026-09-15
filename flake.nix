ab53ec8371f5632ceef1706a7e401b8399842c98
Rezky Yuranda <yurandarezky@atmaluhur.ac.id>
2026-09-15 13:38:17 +0700

    ew lah

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "NixOS + Home Manager configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }:
+    let
+      system = "x86_64-linux";
+    in {
+      nixosConfigurations.catnux = nixpkgs.lib.nixosSystem {
+        inherit system;
+
+        modules = [
+          ./hosts/catnux/configuration.nix
+
+          home-manager.nixosModules.home-manager
+
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+
+            home-manager.users.tabun = import ./home/home.nix;
+          }
+        ];
+      };
+    };
+}
