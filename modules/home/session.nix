cae7d8698bb8ce57475e60c0a00c1e658d546f51
Rezky Yuranda <yurandarezky@atmaluhur.ac.id>
2026-09-17 19:57:24 +0700

    add: android & flutter sdk

---
+++ modules/home/session.nix
@@ -0,0 +1,13 @@
+{}:
+{
+  home.sessionVariables = {
+    ANDROID_HOME = "/data/Apps/sdk/android";
+    ANDROID_SDK_ROOT = "/data/Apps/sdk/android";
+  };
+
+  home.sessionPath = [
+    "/data/Apps/sdk/flutter/bin"
+    "/data/Apps/sdk/android/platform-tools"
+    "/data/Apps/sdk/android/cmdline-tools/latest/bin"
+  ];
+}
